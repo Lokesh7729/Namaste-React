78d8c8496c19e5d638ef3c0cd50ff79102818352
Lokesh <lokeshchauhan7729@gmail.com>
2024-10-27 10:56:08 +0530

    remove mockData

--- 03_talk_is_cheap_show_me_the_code/src/components/Body.js
+++ 03_talk_is_cheap_show_me_the_code/src/components/Body.js
@@ -7,7 +7,7 @@ const Body = () => {
   useEffect(() => {
     fetchData();
   }, []);
-
+s
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.6263908&lng=88.4313014&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
@@ -16,7 +16,7 @@ const Body = () => {
     console.log(
       json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants[0]
         .info.cloudinaryImageId
-    );
+    );b
     const allResList =
       json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
     setListOfRestaurant(allResList);
